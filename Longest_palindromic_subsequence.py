6c5b9afd123a66f20cf9897c08c8d3928b43243f
Noel Benzer <77721002+NoelJB2000@users.noreply.github.com>
2021-11-26 09:35:33 +0530

    Update Longest_palindromic_subsequence.py

--- Longest_palindromic_subsequence.py
+++ Longest_palindromic_subsequence.py
@@ -12,7 +12,7 @@ def lps(S,i,j):
     #if there is only one character in the string then return 1
     if(i==j): 
         return 1
-    #if there is only 2 characters in the string then return 2
+    #if there is only 2 characters in the string and they match then return 2
     if(S[i]==S[j] and i+1==j):
         return 2
     #if the first and last character of the string matches then return the value by adding 2
